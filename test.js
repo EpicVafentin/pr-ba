cbb6e4659ad0317b35a352bf7f147cd236fdea79
Kratky Valentin <yoshi.nyancat@gmail.com>
2021-12-02 08:07:36 +0100

    Project finished

---
+++ test.js
@@ -0,0 +1,7 @@
+import {xd} from "./valami";
+
+describe("Testing xd", () => {
+    test("should return xd", () => {
+        expect(xd()).toBe("xd");
+    })
+})
---
+++ valami.js
@@ -0,0 +1,3 @@
+export function xd() {
+    return "xd";
+}
